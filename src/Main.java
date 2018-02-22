143ffc939a9e10432e4ec02a07e811e6a248f519
borisgalicky <boris.galicky@akademiasovy.sk>
2018-02-22 13:24:57 +0100

    testing and final editations

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,50 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String [] args) throws FileNotFoundException {
-
-        String fileName = "rawText.txt";
+        String filename = "rawText.txt";
         String line = null;
-        PrintWriter writeSPZ = new PrintWriter("spz.txt");
-        PrintWriter writeIDNum = new PrintWriter("rx.txt");
-        PrintWriter writeMACAdress = new PrintWriter("ip.txt");
-        Pattern IDNum = Pattern.compile("^\\d{2}(([0][1-9])|([1][012])|([5][1-9])|([6][012]))([0][1-9]|[12][0-9]|[3][01])[\\/]?\\d{4}$");
-        Pattern SPZ = Pattern.compile("^[A-Z]{2}\\d{3}[A-Z]{2}$");
-        Pattern MAC = Pattern.compile("^([A-F0-9][A-F0-9][:]){5}[A-F0-9][A-F0-9]$");
-
-
+        PrintWriter writespz = new PrintWriter("spz.txt");
+        PrintWriter writerc = new PrintWriter("rc.txt");
+        PrintWriter writemac = new PrintWriter("mac.txt");
+        Pattern rc = Pattern.compile("^\\d{2}(([0][1-9])|([1][012])|([5][1-9])|([6][012]))([0][1-9]|[12][0-9]|[3][01])[\\/]?\\d{4}$");
+        Pattern spz = Pattern.compile("^[A-Z]{2}\\d{3}[A-Z]{2}$");
+        Pattern mac = Pattern.compile("^([A-F0-9][A-F0-9][:]){5}[A-F0-9][A-F0-9]$");
         try {
 
             FileReader fileReader =
-                    new FileReader(fileName);
+                    new FileReader(filename);
             BufferedReader bufferedReader =
                     new BufferedReader(fileReader);
             while((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
                 String []parts = line.split(" ");
                 for(String w : parts) {
-                    Matcher matchSPZ = SPZ.matcher((String)w);
-                    Matcher matchRC = IDNum.matcher((String)w);
-                    Matcher matchMAC = MAC.matcher((String)w);
-
-                    if(matchMAC.matches()){
-                        writeMACAdress.println(w);
+                    Matcher matchspz = spz.matcher((String)w);
+                    Matcher matchrc = rc.matcher((String)w);
+                    Matcher matchmac = mac.matcher((String)w);
+                    if(matchmac.matches()){
+                        writemac.println(w);
                     }
-                    if(matchRC.matches()){
-                        writeIDNum.println(w);
+                    if(matchrc.matches()){
+                        writerc.println(w);
                     }
-                    if(matchSPZ.matches()){
-                        writeSPZ.println(w);
+                    if(matchspz.matches()){
+                        writespz.println(w);
                     }
-
                 }
-
             }
             bufferedReader.close();
-            writeIDNum.close();
-            writeMACAdress.close();
-            writeSPZ.close();
+            writerc.close();
+            writemac.close();
+            writespz.close();
         }
         catch(FileNotFoundException ex) {
             System.out.println(
-                    "Unable to open file '" +
-                            fileName + "'");
+                    "Can't open file '"+filename+"'");
         }
         catch(IOException ex) {
             System.out.println(
-                    "Error reading file '"
-                            + fileName + "'");
+                    "Can't read file '"+filename+"'");
 
         }
     }
